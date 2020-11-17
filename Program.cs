d547c39deb257ac2c8dd5fb96eb8eb13c95aafdc
Lucas <lucasimionato1910@hotmail.com>
2020-11-17 16:19:02 -0200

    subindo arquivos

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lacos_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quer ver a tabuada?");
+            string answer = Console.ReadLine();
+            if (answer == "sim") {
+                for (var j = 0; j <= 10; j++) {
+                    Console.WriteLine("tabuada do " + j);
+                    for (var i = 0; i <= 10; i++) {
+                        Console.WriteLine(j + " x " + i + " = " + (j * i));
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
